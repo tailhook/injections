680d3403f0086e0a94d69604bba0bfcbd9596014
Paul Colomiets <paul@colomiets.name>
2015-06-12 10:28:22 +0300

    Version bumped to v0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='injections',
-      version='0.2',
+      version='0.2.1',
       description='Simple dependency injection library',
       author='Paul Colomiets',
       author_email='paul@colomiets.name',
